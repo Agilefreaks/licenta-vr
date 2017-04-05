ee0da2339d954e1200c79d8665fdc1d6a98cfdad
Mihai <mihai.iftode94@gmail.com>
2017-04-05 20:21:57 +0300

    [#43] Add icon (i) in front of camera

---
+++ Assets/Scripts/HintSystem/ShowHintWindow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHintWindow : MonoBehaviour
+{
+
+    private LineRenderer _lineRenderer;
+    private Transform _cameraLocation;
+
+    public Transform TargetObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        _lineRenderer = gameObject.GetComponentInChildren<LineRenderer>();
+        _cameraLocation = Camera.main.gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_lineRenderer == null) return;
+
+        _lineRenderer.enabled = true;
+        _lineRenderer.SetPosition(0, gameObject.transform.position);
+        _lineRenderer.SetPosition(1, TargetObject.transform.position);
+
+        TargetObject.parent = null;
+        TargetObject.position = new Vector3(_cameraLocation.position.x, _cameraLocation.position.y - 0.3f, _cameraLocation.position.z + 0.5f);
+        TargetObject.eulerAngles = new Vector3(60, 0);
+    }
+}
