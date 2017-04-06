deebfb882d6a3dfd79a964265b61f89ec1baf8f3
Mihai <mihai.iftode94@gmail.com>
2017-04-06 16:13:56 +0300

    [#43] Implement open close events for hint window

---
+++ Assets/Scripts/HintSystem/OpenCloseController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCloseController : MonoBehaviour {
+
+    private LineRenderer _lineRenderer;
+
+    public Transform TargetObject;
+
+    public SteamVR_TrackedObject trackedObject;
+    private SteamVR_Controller.Device device;
+
+    void Start()
+    {
+        _lineRenderer = gameObject.GetComponentInChildren<LineRenderer>();
+        // = GetComponent<SteamVR_TrackedObject>();
+
+    }
+
+    public void OnHintOpen()
+    {
+        if (!_lineRenderer.enabled) _lineRenderer.enabled = true;
+        if (!TargetObject.gameObject.activeSelf) TargetObject.gameObject.SetActive(true);
+        if (TargetObject.parent != null) TargetObject.parent = null;
+    }
+
+    public void OnHintClose()
+    {
+        if (!_lineRenderer.enabled) _lineRenderer.enabled = false;
+        if (!TargetObject.gameObject.activeSelf) TargetObject.gameObject.SetActive(false);
+        if (TargetObject.parent != null) TargetObject.parent = gameObject.transform;
+    }
+
+    private void Update()
+    {
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            OnHintOpen();            
+        }
+    }
+}
--- Assets/Scripts/HintSystem/ShowHintWindow.cs
+++ Assets/Scripts/HintSystem/ShowHintWindow.cs
@@ -14,19 +14,19 @@ public class ShowHintWindow : MonoBehaviour
     void Start()
     {
         _lineRenderer = gameObject.GetComponentInChildren<LineRenderer>();
-        _cameraLocation = Camera.main.gameObject.transform;
+        _cameraLocation = Camera.main.gameObject.transform;        
     }
 
     // Update is called once per frame
     void Update()
     {
         if (_lineRenderer == null) return;
+        
+        
 
-        _lineRenderer.enabled = true;
         _lineRenderer.SetPosition(0, gameObject.transform.position);
         _lineRenderer.SetPosition(1, TargetObject.transform.position);
-
-        TargetObject.parent = null;
+        
         TargetObject.position = new Vector3(_cameraLocation.position.x, _cameraLocation.position.y - 0.3f, _cameraLocation.position.z + 0.5f);
         TargetObject.eulerAngles = new Vector3(60, 0);
     }
