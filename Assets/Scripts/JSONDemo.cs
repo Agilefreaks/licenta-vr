2ae3dd1f5668813a40f4184ed2d0839b75415823
Mihai <mihai.iftode94@gmail.com>
2017-04-06 16:45:58 +0300

    [41] Clear the scene and only leave the three tools on the table
    + Add one more table with 3 tools in the middle of 3D scene
    + Add back colliders and rigidbodys for all tools
    + Add colliders for tables
    [41] Clear the scene and only leave the three tools on the table
    
    + Add one more table with 3 tools in the middle of 3D scene
    + Add back colliders and rigidbodys for all tools
    + Add colliders for tables
    Merge remote-tracking branch 'origin/41' into 41
    [41] Add prefab folder with prefab for scene
    
    + Prefab folder and prefab for Scene3D inside
    Merge remote-tracking branch 'origin/41' into 41
    [41] Change .gitignore
    
    + add *.prefab.meta to be ignored 
    [41] Add json script and json data 
    
    + StreamingAssets folder where is data.json
    + JSONDemo.cs which is drang&drop script to give the description of objects
    [#41] Move Scripts and Prefabs in correct folder

---
+++ Assets/Scripts/JSONDemo.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+public class JSONDemo : MonoBehaviour {
+
+
+    string path;
+    string jsonString;
+
+    // Use this for initialization
+    void Start() {
+        path = Application.streamingAssetsPath + "/data.json";
+        jsonString = File.ReadAllText(path);
+
+        var selectedObject = JsonConvert.DeserializeObject<List<JSONData>>(jsonString);
+        //Debug.Log(getDescritionByName(selectedObject));
+    }
+
+    public string getDescritionByName(List<JSONData> dataList) {
+        foreach (var data in dataList) {
+            if (data.Name.Equals(gameObject.name)) {
+                return data.Description;
+            }
+        }
+        return "";
+    }
+}
+
+public class JSONData {
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
