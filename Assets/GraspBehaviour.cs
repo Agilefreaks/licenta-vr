169575256887ecb8d216e1e3f8c1c0262c2ea98d
Mihai <mihai.iftode94@gmail.com>
2017-04-06 17:20:42 +0300

    [#35] Add leap motion
    [#37] Add leap motion human hands
    
    [#37] Add interaction engine. Interact with an gameobject
    
    [#37] Add grasp behaviour

---
+++ Assets/GraspBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraspBehaviour : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShowMessage()
+    {
+        Debug.Log("Show Message");
+    }
+
+    public void Deactivate()
+    {
+        Debug.Log("Drop the object!");
+    }
+}
